2acdc020a81f298681d662e139ae48ba9fff317a
Oscar Chan <chanoscar0@gmail.com>
2019-04-09 18:08:39 -0700

    Set-up apollo provider with connection to graphql client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 const App = () => {
 
   const [count, setCount] = useState(0)
   return (
     <div>
-      Hello World! {count}
-      <button onClick = {() => setCount(count+1)}>Click me!</button>
+      <ApolloProvider client={client}>
+        Hello World! {count}
+        <button onClick = {() => setCount(count+1)}>Click me!</button>
+      </ApolloProvider>
     </div>
   )
 }
